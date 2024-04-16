fe7078b3a9f4005bb47341d6c8eebd8fec6e8ab2
ATLAS-B28 <adityacbhambere28@gmail.com>
2024-04-16 20:05:46 +0530

    added the code

--- todo/src/main/java/com/example/todo/config/SpringSecConfig.java
+++ todo/src/main/java/com/example/todo/config/SpringSecConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +15,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+//now we will be using @EnableMethodSecurity
+//bean-based configuration for the authorization types.
+@EnableMethodSecurity // enables @preauthorized
 @Configuration
 public class SpringSecConfig {
 
@@ -29,11 +34,19 @@ public class SpringSecConfig {
         //HttpSecurity It allows configuring web-based security for
         // specific http requests. By default, it will be applied to all requests,
         // but can be restricted using #requestMatcher(RequestMatcher) or other similar methods.
-        httpSecurity.csrf(Customizer.withDefaults())//Csrf - Enables CSRF protection. This is activated by default when using EnableWebSecurity.
+        httpSecurity.csrf(csrf -> csrf.disable())//Csrf - Enables CSRF protection. This is activated by default when using EnableWebSecurity.
+                //alternative - AbstractHttpConfigurer::disable
                     .authorizeHttpRequests(//Allows restricting access based upon the HttpServletRequest
                             // using RequestMatcher implementations (i.e., via URL patterns).
                             (authorize) -> {
-                                authorize.requestMatchers(HttpMethod.POST, "/api/v1/todos/create-todo").hasRole("ADMIN");
+                            /*   authorize.requestMatchers(HttpMethod.POST,"/api/v1/todos/create-todo").hasRole("ADMIN");
+                            //    authorize.requestMatchers(HttpMethod.GET,"/api/v1/todos/all").hasAnyRole("ADMIN","USER");
+                            //    authorize.requestMatchers(HttpMethod.GET,"/api/v1/todos/get/{id}").hasAnyRole("ADMIN","USER");
+                            //    authorize.requestMatchers(HttpMethod.DELETE,"/api/v1/todos/delete/{id}").hasRole("ADMIN");
+                            //    authorize.requestMatchers(HttpMethod.PUT,"/api/v1/todos/update/{id}").hasRole("ADMIN");
+                            //    authorize.requestMatchers(HttpMethod.PATCH,"/api/v1/todos/update-status/{id}").hasRole("ADMIN");
+                            //    authorize.requestMatchers(HttpMethod.PATCH,"/api/v1/todos/update-status-incomplete/{id}").hasRole("ADMIN");
+                              */
                                 authorize.anyRequest().authenticated();
                                 //anyRequest - Maps any request.
                                 //Returns:
--- todo/src/main/java/com/example/todo/controller/TodoController.java
+++ todo/src/main/java/com/example/todo/controller/TodoController.java
@@ -5,6 +5,7 @@ import com.example.todo.service.TodoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,36 +17,50 @@ public class TodoController {
 
     private final TodoService todoService;
 
+    @PreAuthorize("hasRole('ADMIN')")
+    /*
+    * Annotation for specifying a method access-control expression
+    * which will be evaluated to decide whether a method invocation
+    * is allowed or not.
+    * */
     @PostMapping("create-todo")
     public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto) {
         return new ResponseEntity<>(todoService.addTodo(todoDto), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("get/{id}")
     public ResponseEntity<TodoDto> getTodoById(@PathVariable Long id) {
         return new ResponseEntity<>(todoService.getTodo(id), HttpStatus.FOUND);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("/all")
     public ResponseEntity<List<TodoDto>> getAllTodo() {
         return new ResponseEntity<>(todoService.getAllTodo(), HttpStatus.FOUND);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteTodo(@PathVariable Long id) {
         return new ResponseEntity<>(todoService.deleteTodo(id), HttpStatus.OK);
     }
 
-    @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/update/{id}")//put sends the whole body in the request
     public ResponseEntity<TodoDto> updateTodo(@PathVariable Long id, @RequestBody TodoDto todoDto) {
         return new ResponseEntity<>(todoService.updateTodo(id, todoDto), HttpStatus.OK);
     }
 
-    @PatchMapping("/update-status/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @PatchMapping("/update-status/{id}")//here a patch used as we update
+    // only one aspect
+    //of resource and so send only that part of resource
     public ResponseEntity<TodoDto> updateTodoStatus(@PathVariable Long id) {
         return new ResponseEntity<>(todoService.completeTodo(id), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping("/update-status-incomplete/{id}")
     public ResponseEntity<TodoDto> updateTodoStatusInComplete(@PathVariable Long id) {
         return new ResponseEntity<>(todoService.inCompleteTodo(id), HttpStatus.OK);
