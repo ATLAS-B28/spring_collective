79aa67a708644971cbeccaea0154d56a67585df0
ATLAS-B28 <adityacbhambere28@gmail.com>
2024-06-18 12:49:11 +0530

    Adding the wells one to and will work on graphql project

---
+++ graphical/src/main/java/com/example/graphical/GraphicalApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphicalApplication.class, args);
+	}
+
+}
---
+++ wells_forage/src/test/java/com/example/wells_forage/WellsForageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wells_forage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellsForageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
