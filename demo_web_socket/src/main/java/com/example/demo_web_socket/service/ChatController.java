5a784a20f1dc5fbb5710e0df423933b6c1693145
ATLAS-B28 <adityacbhambere28@gmail.com>
2024-07-17 16:29:52 +0530

    Added websocket corrections

--- demo_web_socket/src/main/java/com/example/demo_web_socket/service/ChatController.java
+++ demo_web_socket/src/main/java/com/example/demo_web_socket/service/ChatController.java
@@ -6,6 +6,8 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+import java.util.Objects;
+
 @Controller
 public class ChatController {
 
@@ -18,7 +20,7 @@ public class ChatController {
     @MessageMapping("chat.addUser")
     @SendTo("/topic/chat")
     public Message addUser(@Payload Message msg, SimpMessageHeaderAccessor headerAccessor) {
-        headerAccessor.getSessionAttributes().put("username", msg.getSender());
+        Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", msg.getSender());
         return msg;
     }
 }
