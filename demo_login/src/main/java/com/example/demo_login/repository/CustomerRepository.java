126e3647c0b32d14edf5a0840f30859e5c972505
ATLAS-B28 <adityacbhambere28@gmail.com>
2024-07-22 11:32:57 +0530

    Adding HTMX

--- demo_login/src/main/java/com/example/demo_login/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo_login.repository;
-
-import com.example.demo_login.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Customer findByEmail(String email);
-}
