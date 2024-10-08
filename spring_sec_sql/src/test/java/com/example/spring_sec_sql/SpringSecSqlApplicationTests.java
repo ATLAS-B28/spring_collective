5770acfcd58f0e1d59f840af8c8a930dd7eb80dd
ATLAS-B28 <adityacbhambere28@gmail.com>
2024-10-08 13:52:38 +0530

    adding new project

---
+++ spring_sec_sql/src/test/java/com/example/spring_sec_sql/SpringSecSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_sec_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
